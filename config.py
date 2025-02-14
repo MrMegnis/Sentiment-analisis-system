7c844b7821166cea36985292e4e69a6cfa806cd3
MrMegnis <mr.megnis@mail.ru>
2025-02-15 01:49:24 +0300

    add simple web-app for models

---
+++ config.py
@@ -0,0 +1,8 @@
+MODELS = {
+    "RuBERT-tiny2": "Megnis/rubert-tiny2-sentiment-analisys-2",  # замените на ваш id модели
+    "RuBERT-tiny": "Megnis/rubert-tiny-sentiment-analisys",
+    "DistilBERT": "Megnis/distilbert-base-multilingual-cased-sentiment-analisys"
+}
+
+UPLOAD_FOLDER = "uploads"
+ALLOWED_EXTENSIONS = {"csv", "xlsx"}
